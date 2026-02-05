56f2d1ea1b27d0555818e0ee6209c17979eadd72
Kaje1404 <faezeh.a.8416@gmail.com>
2026-02-05 12:10:09 +0330

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,6 @@
+print("Hello Kaje Nasir University!)
+      print("This is my first Pyton projekt for changing major.")
+      number1 = float(input("Enter first number: "))
+number2 = flout(input("Enter second number: ")
+print("Addition result:",number1 + number2)
+print("subtraction result:", 
